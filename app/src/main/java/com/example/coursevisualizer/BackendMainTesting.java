668ee8698235c0b8baec63c0b9c922fbbff907d9
kapoooya2001 <55564765+kapoooya2001@users.noreply.github.com>
2020-05-07 01:48:28 -0700

    -added FireApp class -implemented Firebase into the project -implemented a temporary log in screen - will need further testing amd debugging

--- app/src/main/java/com/example/coursevisualizer/BackendMainTesting.java
+++ app/src/main/java/com/example/coursevisualizer/BackendMainTesting.java
@@ -1,7 +1,7 @@
-package com.example.coursevisualizer;
-
-public class BackendMainTesting {
-    public static void main(String[] args) {
-        System.out.println(100.000 == 100);
-    }
-}
+//package com.example.coursevisualizer;
+//
+//public class BackendMainTesting {
+//    public static void main(String[] args) {
+//        System.out.println(100.000 == 100);
+//    }
+//}
